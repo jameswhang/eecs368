988a5ed430c19196b70311293e48acfc34d497d3
James Whang <sungyoonwhang2017@u.northwestern.edu>
2015-02-26 22:28:45 -0600

    added cde

--- lab3/opt_2dhisto.h
+++ lab3/opt_2dhisto.h
@@ -1,7 +1,15 @@
 #ifndef OPT_KERNEL
 #define OPT_KERNEL
 
-void opt_2dhisto( /*Define your own function parameters*/ );
+void opt_2dhisto(uint32_t * input, size_t height, size_t width, uint8_t * bins);
+
+void * AllocateDevice(size_t size);
+
+void CopyToDevice(void * D_device, void * D_host, size_t size);
+
+void CopyFromDevice(void * D_host, void * D_device, size_t size);
+
+void FreeDevice(void * D_device); 
 
 /* Include below the function headers of any other functions that you implement */
 
