cac9e1b05ecd4c2c0da4ff442b39c6fda61949f1
Nevil George <nevil@u.northwestern.edu>
2015-03-18 23:59:57 -0500

    coalescing tests are passing but times are getting slower as more memory accesses are done at once

--- lab3/opt_2dhisto.cu
+++ lab3/opt_2dhisto.cu
@@ -17,22 +17,6 @@ void opt_2dhisto(uint32_t* input, size_t height, size_t width, uint8_t* bins, ui
        histogramming kernel. Any memory allocations and
        transfers must be done outside this function */
 
-	/*
-	cudaMemset(bins, 0, HISTO_HEIGHT * HISTO_WIDTH * sizeof(bins[0]));
-	cudaMemset(g_bins, 0, HISTO_HEIGHT * HISTO_WIDTH * sizeof(g_bins[0]));
-	
-	// kernel allocates bins
-	// using 1024 * T threads so that more steam multiprocessors can be used
-	opt_2dhistoKernel<<<2 * T, 512>>>(input, height, width, g_bins);
-	cudaThreadSynchronize();
-
-
-	// convert 32 bits to 8 bits
-	opt_32to8Kernel<<<HISTO_HEIGHT * HISTO_WIDTH / 512, 512>>>(g_bins, bins, 1024);
-
-	cudaThreadSynchronize();
-	*/
-
 	// working
 	dim3 block(16, 16);
 	dim3 grid(((INPUT_WIDTH + 128) & 0xFFFFFF80) / 16, INPUT_HEIGHT / 16);
@@ -49,28 +33,6 @@ void opt_2dhisto(uint32_t* input, size_t height, size_t width, uint8_t* bins, ui
 /* kernel verson 1: basic */
 __global__ void opt_2dhistoKernel(uint32_t *input, size_t height, size_t width, uint32_t* bins){
 
-	/*
-	// put sub-histogram in shared memory
-	__shared__ int sub_hist[1024];
-
-	// Avoid bank conflicts by accessing shared memory sequentially
-	sub_hist[threadIdx.x] = 0;
-	sub_hist[threadIdx.x + 512] = 0;
-
-	// global thread id
-	int index = blockDim.x * blockIdx.x + threadIdx.x;
-
-	for (int i = 0; i < (width / T); i++) {
-		atomicAdd(sub_hist + input[((index / T) * height) + ((index % T) * (width / T)) + i], 1);
-	}
-	__syncthreads();
-
-	// Update the global memory
-	// Writing is also sequential to avoid bank conflicts
-	atomicAdd(bins + threadIdx.x, sub_hist[threadIdx.x]);
-	atomicAdd(bins + threadIdx.x, sub_hist[threadIdx.x + 512]);
-	*/
-	
 	/*	
 	// working
 	int col = blockDim.x * blockIdx.x + threadIdx.x;
@@ -85,8 +47,10 @@ __global__ void opt_2dhistoKernel(uint32_t *input, size_t height, size_t width, 
 		atomicAdd(&bins[input[col + row * ((INPUT_WIDTH + 128) & 0xFFFFFF80)]], 1);
 	}
 	*/
+
+	/*	
+	// working
 	__shared__ int temp[1024];
-	
 	temp[threadIdx.x] = 0;
 	__syncthreads();
 
@@ -101,6 +65,30 @@ __global__ void opt_2dhistoKernel(uint32_t *input, size_t height, size_t width, 
 	__syncthreads();
 
 	atomicAdd(&(bins[threadIdx.x]), temp[threadIdx.x]);	
+	*/
+
+	// working
+	__shared__ int temp[1024];
+	temp[threadIdx.x] = 0;
+	temp[threadIdx.x + 256] = 0;
+	temp[threadIdx.x + 512] = 0;
+	temp[threadIdx.x + 768] = 0;
+	__syncthreads();
+
+	int i = threadIdx.x + blockIdx.x * blockDim.x;
+	int offset = blockDim.x * gridDim.x;
+	int size = HISTO_HEIGHT * HISTO_WIDTH;
+
+	while (i < size) {
+		atomicAdd(&temp[input[i]], 1);
+		i += offset;
+	} 
+	__syncthreads();
+
+	atomicAdd(&(bins[threadIdx.x]), temp[threadIdx.x]);
+	atomicAdd(&(bins[threadIdx.x + 256]), temp[threadIdx.x + 256]);
+	atomicAdd(&(bins[threadIdx.x + 512]), temp[threadIdx.x + 512]);
+	atomicAdd(&(bins[threadIdx.x + 768]), temp[threadIdx.x + 768]);
 	
 }
 
